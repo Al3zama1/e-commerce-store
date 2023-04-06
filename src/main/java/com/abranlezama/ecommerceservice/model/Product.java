fe115c314e6c1da97694fd80e88ae6ed360b6871
Abran Lezama <lezama.abran@gmail.com>
2023-04-05 20:16:28 -0700

    Implemented Product entity class

---
+++ src/main/java/com/abranlezama/ecommerceservice/model/Product.java
@@ -0,0 +1,17 @@
+package com.abranlezama.ecommerceservice.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long product_id;
+    @Column(nullable = false, length = 20)
+    private String name;
+    @Column(length = 100)
+    private String description;
+    @Column(nullable = false)
+    private double price;
+    private int stock_quantity;
+}
