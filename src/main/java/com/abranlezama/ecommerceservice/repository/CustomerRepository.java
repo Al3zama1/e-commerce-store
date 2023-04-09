8a12f4934fd7367ba70c7e26a080d5aed3f40953
Abran Lezama <lezama.abran@gmail.com>
2023-04-08 23:14:36 -0700

    Implemented Customer model
    - Implemented Customer model class.
    - Implemented CustomerRepository to access database data.
    - Implemented Flyway Customer migration that creates customer tabe in
      database.

---
+++ src/main/java/com/abranlezama/ecommerceservice/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.abranlezama.ecommerceservice.repository;
+
+import com.abranlezama.ecommerceservice.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByUserId(long userId);
+}
