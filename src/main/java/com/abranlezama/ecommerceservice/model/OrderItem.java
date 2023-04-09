3f4d20fd71254edbcff0220cbea1019a836bd603
Abran Lezama <lezama.abran@gmail.com>
2023-04-09 12:46:06 -0700

    Added equals and hashcode methods to OrderItem model

--- src/main/java/com/abranlezama/ecommerceservice/model/OrderItem.java
+++ src/main/java/com/abranlezama/ecommerceservice/model/OrderItem.java
@@ -3,6 +3,8 @@ package com.abranlezama.ecommerceservice.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Objects;
+
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
@@ -27,4 +29,19 @@ public class OrderItem {
     @ManyToOne
     @JoinColumn(name = "product_id", referencedColumnName = "id")
     private Product product;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id) && Objects.equals(quantity, orderItem.quantity) &&
+                Objects.equals(price, orderItem.price) && Objects.equals(order, orderItem.order) &&
+                Objects.equals(product, orderItem.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, quantity, price, order, product);
+    }
 }
