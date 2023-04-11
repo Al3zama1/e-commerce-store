8c9a90282db58977b32c0567d8f2b24901150b0d
Abran Lezama <lezama.abran@gmail.com>
2023-04-10 18:01:20 -0700

    Updated AuthenticationControllerTest
    Modified test class to only be applicable to the authentication
    controller.

--- src/test/java/com/abranlezama/ecommerceservice/controller/AuthenticationControllerTest.java
+++ src/test/java/com/abranlezama/ecommerceservice/controller/AuthenticationControllerTest.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@WebMvcTest
+@WebMvcTest(controllers = {AuthenticationController.class})
 @Import(value = {JwtService.class, SecurityConfig.class, CustomAuthenticationEntryPoint.class})
 class AuthenticationControllerTest {
 
