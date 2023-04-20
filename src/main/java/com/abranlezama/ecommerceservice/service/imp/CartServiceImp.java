4c362064a6f8676df22e0a2b66a949cf20fafc93
Abran Lezama <lezama.abran@gmail.com>
2023-04-20 12:40:41 -0700

    Implemented cart service logic to add item to cart
    Created implementation of method and tested for different edge cases.
    - Tested for existence of product.
    - Tested for product stock availability.
    - Tested to verify if product was already iin customer's shopping cart.

--- src/main/java/com/abranlezama/ecommerceservice/service/imp/CartServiceImp.java
+++ src/main/java/com/abranlezama/ecommerceservice/service/imp/CartServiceImp.java
@@ -2,11 +2,15 @@ package com.abranlezama.ecommerceservice.service.imp;
 
 import com.abranlezama.ecommerceservice.dto.cart.AddItemToCartDto;
 import com.abranlezama.ecommerceservice.dto.cart.CartDto;
+import com.abranlezama.ecommerceservice.exception.ExceptionMessages;
+import com.abranlezama.ecommerceservice.exception.ProductNotFoundException;
+import com.abranlezama.ecommerceservice.exception.ProductOutOfStockException;
 import com.abranlezama.ecommerceservice.mapstruct.mapper.CartMapper;
-import com.abranlezama.ecommerceservice.model.Cart;
-import com.abranlezama.ecommerceservice.model.Customer;
+import com.abranlezama.ecommerceservice.model.*;
+import com.abranlezama.ecommerceservice.repository.CartItemRepository;
 import com.abranlezama.ecommerceservice.repository.CartRepository;
 import com.abranlezama.ecommerceservice.repository.CustomerRepository;
+import com.abranlezama.ecommerceservice.repository.ProductRepository;
 import com.abranlezama.ecommerceservice.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,6 +24,8 @@ public class CartServiceImp implements CartService {
     private final CartRepository cartRepository;
     private final CustomerRepository customerRepository;
     private final CartMapper cartMapper;
+    private final ProductRepository productRepository;
+    private final CartItemRepository cartItemRepository;
 
     @Override
     public CartDto getCartItems(long userId) {
@@ -29,7 +35,46 @@ public class CartServiceImp implements CartService {
 
     @Override
     public void addItemToShoppingCart(long userId, AddItemToCartDto addItemToCartDto) {
+        // Get customer's shopping cart
+        Cart cart = cartRepository.findByCustomer_User_Id(userId);
+        // Get product that will be added to cart
+        Product product = productRepository.findById(addItemToCartDto.productId())
+                .orElseThrow(() -> new ProductNotFoundException(ExceptionMessages.PRODUCT_NOT_FOUND));
 
+        // check if product is already in customer's cart
+        Optional<CartItem> cartItemOptional = cart.getCartItems().stream()
+                .filter(cartItem -> cartItem.getProduct().getId().equals(product.getId()))
+                .findFirst();
+
+        CartItem cartItem;
+
+        if (cartItemOptional.isPresent()) {
+            cartItem = cartItemOptional.get();
+            cartItem.setQuantity((short) (cartItem.getQuantity() + addItemToCartDto.quantity()));
+        } else {
+            cartItem = CartItem.builder().cart(cart).quantity(addItemToCartDto.quantity())
+                    .product(product).build();
+            cart.getCartItems().add(cartItem);
+        }
+
+        // verify wanted quantity is in stock
+        if (!checkProductAvailability(cartItem, addItemToCartDto.quantity())) throw new
+                ProductOutOfStockException(ExceptionMessages.PRODUCT_OUT_OF_STOCK);
+
+        // calculate cart total
+        cart.setTotalCost(calculateCartTotal(cart));
+
+        cartRepository.save(cart);
+    }
+
+    private float calculateCartTotal(Cart cart) {
+        return cart.getCartItems().stream()
+                .map(item -> item.getProduct().getPrice() * item.getQuantity())
+                .reduce(0F, Float::sum);
+    }
+
+    private boolean checkProductAvailability(CartItem cartItem, short wanted) {
+        return cartItem.getProduct().getStockQuantity() >= wanted;
     }
 
     @Override
--- src/test/java/com/abranlezama/ecommerceservice/service/imp/CartServiceImpTest.java
+++ src/test/java/com/abranlezama/ecommerceservice/service/imp/CartServiceImpTest.java
@@ -1,24 +1,55 @@
 package com.abranlezama.ecommerceservice.service.imp;
 
+import com.abranlezama.ecommerceservice.dto.cart.AddItemToCartDto;
+import com.abranlezama.ecommerceservice.exception.ExceptionMessages;
+import com.abranlezama.ecommerceservice.exception.ProductNotFoundException;
+import com.abranlezama.ecommerceservice.exception.ProductOutOfStockException;
 import com.abranlezama.ecommerceservice.mapstruct.mapper.CartMapper;
+import com.abranlezama.ecommerceservice.model.Cart;
+import com.abranlezama.ecommerceservice.model.CartItem;
+import com.abranlezama.ecommerceservice.model.CartItemPK;
+import com.abranlezama.ecommerceservice.model.Product;
+import com.abranlezama.ecommerceservice.objectmother.CartMother;
+import com.abranlezama.ecommerceservice.objectmother.ProductMother;
+import com.abranlezama.ecommerceservice.repository.CartItemRepository;
 import com.abranlezama.ecommerceservice.repository.CartRepository;
+import com.abranlezama.ecommerceservice.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.never;
 
 @ExtendWith(SpringExtension.class)
 class CartServiceImpTest {
     @Mock
     private CartRepository cartRepository;
     @Mock
+    private CartItemRepository cartItemRepository;
+    @Mock
+    private ProductRepository productRepository;
+    @Mock
     private CartMapper cartMapper;
 
     @InjectMocks
     private CartServiceImp cut;
+    @Captor
+    private ArgumentCaptor<CartItem> cartItemArgumentCaptor;
+    @Captor
+    private ArgumentCaptor<Cart> cartArgumentCaptor;
 
     @Test
     void shouldReturnCustomerCartItems() {
@@ -30,6 +61,97 @@ class CartServiceImpTest {
 
         // When
         then(cartRepository).should().findByCustomer_User_Id(userId);
+    }
+
+
+    @Test
+    void shouldAddProductToUserShoppingCart() {
+        // Given
+        long userId = 1L;
+        AddItemToCartDto addItemToCartDto = AddItemToCartDto.builder()
+                .productId(1L).quantity((short) 2)
+                .build();
+        Product product = ProductMother.saveProduct().id(addItemToCartDto.productId()).build();
+        Cart cart = CartMother.cart().totalCost(500F).cartItems(new ArrayList<>()).build();
+
+        given(cartRepository.findByCustomer_User_Id(userId)).willReturn(cart);
+        given(productRepository.findById(addItemToCartDto.productId())).willReturn(Optional.of(product));
+
+        // When
+        cut.addItemToShoppingCart(userId, addItemToCartDto);
+
+        // Then
+        then(cartRepository).should().save(cartArgumentCaptor.capture());
+        assertThat(cartArgumentCaptor.getValue().getTotalCost()).isEqualTo(1000F);
+    }
+
+    @Test
+    void shouldThrowProductNotFoundExceptionWhenAddingNonExistingProductToCart() {
+        // Given
+        long userId = 1L;
+        AddItemToCartDto addItemToCartDto = AddItemToCartDto.builder()
+                .productId(1L).quantity((short) 2)
+                .build();
+
+        given(cartRepository.findByCustomer_User_Id(userId)).willReturn(new Cart());
+        given(productRepository.findById(addItemToCartDto.productId())).willReturn(Optional.empty());
+
+        // When
+        assertThatThrownBy(() -> cut.addItemToShoppingCart(userId, addItemToCartDto))
+                .hasMessage(ExceptionMessages.PRODUCT_NOT_FOUND)
+                .isInstanceOf(ProductNotFoundException.class);
+
+        // Then
+        then(cartItemRepository).shouldHaveNoInteractions();
+    }
+
+    @Test
+    void shouldIncrementCartItemProductQuantityWhenItIsAlreadyInCustomerCart() {
+        // Given
+        long userId = 1L;
+        AddItemToCartDto addItemToCartDto = AddItemToCartDto.builder()
+                .productId(1L).quantity((short) 2)
+                .build();
+        Product product = ProductMother.saveProduct().id(addItemToCartDto.productId()).build();
+        Cart cart = CartMother.cart().totalCost(500F).build();
+        CartItem cartItem = CartMother.cartItem().product(product).cart(cart).build();
+        cart.setCartItems(List.of(cartItem));
+
+
+        given(cartRepository.findByCustomer_User_Id(userId)).willReturn(cart);
+        given(productRepository.findById(addItemToCartDto.productId())).willReturn(Optional.of(product));
+
+        // When
+        cut.addItemToShoppingCart(userId, addItemToCartDto);
+
+        // Then
+        then(cartRepository).should().save(cartArgumentCaptor.capture());
+        assertThat(cartArgumentCaptor.getValue().getTotalCost()).isEqualTo(1500F);
+        assertThat(cart.getCartItems().get(0).getQuantity()).isEqualTo((short) 3);
+    }
+
+    @Test
+    void shouldThrowProductOutOfStockExceptionWhenWantedQuantityIsGreaterThanAvailable() {
+        // Given
+        long userId = 1L;
+        AddItemToCartDto addItemToCartDto = AddItemToCartDto.builder()
+                .productId(1L).quantity((short) 2).build();
+        Product product = ProductMother.saveProduct().id(addItemToCartDto.productId()).stockQuantity(1).build();
+        Cart cart = CartMother.cart().totalCost(500F).build();
+        CartItem cartItem = CartMother.cartItem().product(product).cart(cart).build();
+        cart.setCartItems(List.of(cartItem));
+
+
+        given(cartRepository.findByCustomer_User_Id(userId)).willReturn(cart);
+        given(productRepository.findById(addItemToCartDto.productId())).willReturn(Optional.of(product));
+
+        // When
+        assertThatThrownBy(() -> cut.addItemToShoppingCart(userId, addItemToCartDto))
+                .hasMessage(ExceptionMessages.PRODUCT_OUT_OF_STOCK)
+                .isInstanceOf(ProductOutOfStockException.class);
+
+        // Then
+        then(cartRepository).should(never()).save(any());
     }
 
 }
