72775b1c6ffb308842aed55f07226c5407618845
Abran Lezama <lezama.abran@gmail.com>
2023-04-19 21:06:27 -0700

    Added equals and hash code to CartItem model

--- src/main/java/com/abranlezama/ecommerceservice/model/CartItem.java
+++ src/main/java/com/abranlezama/ecommerceservice/model/CartItem.java
@@ -3,6 +3,8 @@ package com.abranlezama.ecommerceservice.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = {"product_id", "cart_id"}))
 @IdClass(CartItemPK.class)
@@ -27,4 +29,19 @@ public class CartItem {
 
     @Column(nullable = false)
     private Short quantity;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CartItem cartItem = (CartItem) o;
+        return Objects.equals(product, cartItem.product) && Objects.equals(cart, cartItem.cart) &&
+                Objects.equals(quantity, cartItem.quantity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(product, cart, quantity);
+    }
 }
