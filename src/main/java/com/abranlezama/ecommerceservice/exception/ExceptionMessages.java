db2dd1ad6aec9af3650018f0c4bb43101bc6a228
Abran Lezama <lezama.abran@gmail.com>
2023-04-20 15:08:03 -0700

    Added message to be thrown when trying to update non existing cart item

--- src/main/java/com/abranlezama/ecommerceservice/exception/ExceptionMessages.java
+++ src/main/java/com/abranlezama/ecommerceservice/exception/ExceptionMessages.java
@@ -1,9 +1,12 @@
 package com.abranlezama.ecommerceservice.exception;
 
 public class ExceptionMessages {
+
+    private ExceptionMessages() {}
     public static final String FAILED_AUTHENTICATION = "Password or username do not match our records";
     public static final String USER_ALREADY_EXISTS = "Email taken";
     public static final String PASSWORDS_MUST_MATCH = "Passwords must match";
     public static final String PRODUCT_NOT_FOUND = "Product not found";
+    public static final String PRODUCT_NOT_IN_CART = "Product is not in cart";
     public static final String PRODUCT_OUT_OF_STOCK = "Sorry, product is out of stock";
 }
